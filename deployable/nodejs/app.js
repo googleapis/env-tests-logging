ec7b2bf38b45217bd059246c40c10f03051dc2e7
nicoleczhu <nicolezhu@google.com>
2021-03-19 15:50:36 -0700

    implemented simplelog for node

--- deployable/nodejs/app.js
+++ deployable/nodejs/app.js
@@ -10,18 +10,36 @@ const logging = new Logging();
  * @param {object} context The event metadata.
  */
 exports.pubsubFunction = (message, context) => {
-  console.log("success yayyyy");
-
   const msg = message.data
       ? Buffer.from(message.data, 'base64').toString()
-      : 'World';
+      : console.log("no log function was invoked");
 
-  console.log(`Message was: ${msg}!`);
+  console.log('attributes if any: ');
+  console.log(message.attributes);
 
-  // TODO deflake this if needed
-  const log = logging.log("mylog");
+  // TODO later (nicolezhu):
+  // write fns in separate file and do var funcFo0 = function(){}... modules.exports={ func: funcFoo}
+  // var methods = require()... methods['funcString']()
+  switch (msg) {
+    case 'simplelog':
+      if (message.attributes) {
+        simplelog(message.attributes['log_name'], message.attributes['log_text']);
+      } else {
+        simplelog();
+      }
+      break;
+    default:
+      console.log(`Invalid log function was invoked.`);
+  }
+};
 
-  const text_entry = log.entry('LogEntry: Hello world!');
+/**
+ * envctl nodejs <env> trigger simplelog log_name=foo,log_text=bar
+ */
+function simplelog(logname = "my-log", logtext = "hello world" ) {
+  const log = logging.log(logname);
+
+  const text_entry = log.entry(logtext);
 
   log.write(text_entry).then(r => console.log(r));
-};
+}
