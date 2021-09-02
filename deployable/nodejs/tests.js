5def568dab55cd881d592b99acbdba30f205639f
Daniel Sanche <d.sanche14@gmail.com>
2021-09-02 15:06:16 -0700

    fixed typo

--- deployable/nodejs/tests.js
+++ deployable/nodejs/tests.js
@@ -23,7 +23,7 @@ var simplelog = function(args) {
   // set default values
   const logname = "logname" in args ? args["logname"] : "my-log";
   const logtext = "log_text" in args ? args["log_text"] : "simplelog";
-  const seveverity = "severity" in args ? args["severity"] : "ERROR";
+  const severity = "severity" in args ? args["severity"] : "ERROR";
 
   const log = logging.log(logname);
 
