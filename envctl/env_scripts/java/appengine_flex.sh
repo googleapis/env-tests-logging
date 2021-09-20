dc64a086a13f30629e983303a1addbf2897943f2
Daniel Sanche <d.sanche14@gmail.com>
2021-09-20 12:50:55 -0700

    removed test code

--- envctl/env_scripts/java/appengine_flex.sh
+++ envctl/env_scripts/java/appengine_flex.sh
@@ -48,10 +48,6 @@ verify() {
 
 
 deploy() {
-  # update health checks
-  set -x
-  gcloud app update --split-health-checks --project gcloud-devel
-  set +x
   # create pub/sub topic
   set +e
   gcloud pubsub topics create $SERVICE_NAME 2>/dev/null
