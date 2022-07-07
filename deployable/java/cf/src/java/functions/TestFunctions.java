42da63a46c121f111ffd0e1aedc54a32d9c4aff5
minherz <minherz@gmail.com>
2022-07-07 00:50:46 +0000

    testing: fix the CF env test for Java

--- deployable/java/cf/src/java/functions/TestFunctions.java
+++ deployable/java/cf/src/java/functions/TestFunctions.java
@@ -1,33 +1,27 @@
 package functions;
 
-import com.google.cloud.functions.BackgroundFunction;
-import com.google.cloud.functions.Context;
-import com.google.events.cloud.pubsub.v1.Message;
-import com.google.cloud.MonitoredResource;
+import com.google.cloud.functions.CloudEventsFunction;
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.Logging;
 import com.google.cloud.logging.LoggingOptions;
 import com.google.cloud.logging.Payload.StringPayload;
 import com.google.cloud.logging.Severity;
+import com.google.cloud.logging.Synchronicity;
+import io.cloudevents.CloudEvent;
 import java.util.Collections;
 
-public class TestFunctions implements BackgroundFunction<Message> {
-
+public class TestFunctions implements CloudEventsFunction {
+  private static final Logging logging = LoggingOptions.getDefaultInstance().getService();
+  
   @Override
-  public void accept(Message message, Context context) {
-    String payload = "hello world";
-    if (message != null && message.getData() != null) {
-      payload = new String(
-          Base64.getDecoder().decode(message.getData().getBytes(StandardCharsets.UTF_8)),
-          StandardCharsets.UTF_8);
-    }
-    try (Logging logging = LoggingOptions.getDefaultInstance().getService()) {
-        logging.setWriteSynchronicity(Synchronicity.SYNC);
+  public void accept(CloudEvent event) {
+    // Get cloud event data as JSON string
+    String cloudEventData = new String(event.getData().toBytes());
+    logging.setWriteSynchronicity(Synchronicity.SYNC);
 
-        LogEntry entry = LogEntry.newBuilder(StringPayload.of(payload))
-            .setLogName("test-log")
-            .build();
-        logging.write(Collections.singleton(entry));
-    }
+    LogEntry entry = LogEntry.newBuilder(StringPayload.of(cloudEventData))
+        .setLogName("test-log")
+        .build();
+    logging.write(Collections.singleton(entry));
   }
 }
