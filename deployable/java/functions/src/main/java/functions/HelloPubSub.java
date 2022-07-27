9d059d8d1e3046735bb3c0a004a7bea220f03731
Daniel Sanche <d.sanche14@gmail.com>
2022-07-27 14:51:24 -0600

    set default value for arguments map

--- deployable/java/functions/src/main/java/functions/HelloPubSub.java
+++ deployable/java/functions/src/main/java/functions/HelloPubSub.java
@@ -26,6 +26,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import java.util.Map;
+import java.util.HashMap;
 import java.nio.charset.StandardCharsets;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -37,7 +38,9 @@ public class HelloPubSub implements BackgroundFunction<PubSubMessage> {
   public void accept(PubSubMessage message, Context context) {
     String fnName = new String(Base64.getDecoder().decode(message.data.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
     Map<String, String> args = message.attributes;
-
+    if (args == null){
+        args =  new HashMap<String, String>();
+    }
     triggerSnippet(fnName, args);
     return;
   }
@@ -49,7 +52,7 @@ public class HelloPubSub implements BackgroundFunction<PubSubMessage> {
       Method found = c.getDeclaredMethod(fnName, new Class[] {Map.class});
       found.invoke(obj, args);
     } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-      System.out.println(e.toString());
+      e.printStackTrace();
     }
   }
 }
